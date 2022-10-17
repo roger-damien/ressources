3f0dfcc5994ac0b281b70f40356287f45c82812b
rogers75017 <37438700+rogers75017@users.noreply.github.com>
2022-10-17 13:19:25 +0200

    20221017

--- 20221011_3_DEMO__WRAPPER_boxing_unboxing/Demo04_WRAPPER_Boxing_Unboxing__EXO06.java
+++ 20221011_3_DEMO__WRAPPER_boxing_unboxing/Demo04_WRAPPER_Boxing_Unboxing__EXO06.java
@@ -28,32 +28,18 @@ public static void main(String[] args)
 		BigDecimal  --> double
 		*/
 
-	//////////////////////////////////////////////
+	////////////////////////////////////////////////////////
 	//  METHODES STATIQUES DE LA CLASSE D'EMBALLAGE Integer
-	//////////////////////////////////////////////
-	
-	//méthodes statiques de Integer
+	////////////////////////////////////////////////////////
+	// Integer.
 	int  j = Integer.parseInt("17");
+	System.out.println("127 vaut en hexadécimal :" + Integer.toHexString(127));
+	Integer j2 = Integer.valueOf(j);//on verra aussi l'auto-boxing --> Integer j3 = j;
 	
-	//remplace constructeur déprécié
-	//Integer emballage = new Integer(j);
-	
-	Integer emballage6 = 23;//auto-boxing
-	Integer emballage = Integer.valueOf(j);//Java Factory Methods // of , valueOf , getInstance
-	//On nous éloigne de l'implémentation
-	
-	System.out.println("toString d'un objet Integer :" + emballage);
-	//System.out.println("toString d'un objet Integer :" + emballage.toString() );
-	
-	//Integer emballage2  ="17";  //auto-boxing sur une chaine ne marche pas
-	Integer emballage2  = Integer.valueOf("17");
-	
-	System.out.println();
-	
-	//////////////////////////////////////////////
+	//////////////////////////////////////////////////////////
 	//  METHODES STATIQUES DE LA CLASSE D'EMBALLAGE Character
-	//////////////////////////////////////////////
-	//méthodes statiques de Character
+	//////////////////////////////////////////////////////////
+	//Character.
 	System.out.println("Character.isDigit('?') :" + Character.isDigit('?'));
 	System.out.println("Character.isDigit('9') :" + Character.isDigit('9'));
 	System.out.println("Character.isLowerCase('A') :" + Character.isLowerCase('A') );
@@ -65,52 +51,83 @@ public static void main(String[] args)
 	else
 		System.out.println("Lettre ne pouvant pas être présente dans un identificateur Java");
 	//Character.isJavaIdentifierPart(codePoint)
+	
+	System.out.println();
+	System.out.println();		
 
-
-	//////////////////////////////////////////////
+	//////////////////////////////////////////////////////
 	//  Boxing,  EMBALLAGE
-	//////////////////////////////////////////////
-	//boxing : emballage dans un objet wrapper
+	//////////////////////////////////////////////////////
+	//boxing : emballage du primitif dans un objet wrapper
 	
-	//Integer o = new Integer(4);//déprécié
+	//Le constructeur du wrapper est déprécié
+	//Integer emballage = new Integer(j);
+	//Integer o = new Integer(4);
 	
-	Integer objInteger = Integer.valueOf(4);
-
-	System.out.println("o : " + objInteger );
-	
-	//  AUTOMATIQUE
 	//boxing automatique (auto-boxing), à utiliser pour alléger l'écriture
+	Integer emballage6 = 23;
 	Integer o2 = 4;
 	System.out.println("o2 : " + o2 );
-
+	
+	//autre possibilité que l'auto-boxing, le boxing grâce
+	//à une Java Factory Methods de type   of , valueOf , getInstance
+	Integer emballage = Integer.valueOf(j);//On nous éloigne de l'implémentation
+	Integer objInteger = Integer.valueOf(4);
+	System.out.println("o : " + objInteger );
+	
+	System.out.println("le toString() d'un objet ... Integer :" + emballage);
+	//emballage.toString()
+	
+	//Integer emballage2  ="17";  //l'auto-boxing sur une chaine ne marche pas
+	Integer emballage2  = Integer.valueOf("17");
+	
+	System.out.println();
+	System.out.println();		
 	//////////////////////////////////////////////
-	//  unboxing,  déballage
+	//  Unboxing,  DEBALLAGE
 	//////////////////////////////////////////////
 	//unboxing : déballage d'un objet wrapper
+	
+	//unboxing automatique : auto-unboxing ,  à utiliser pour alléger l'écriture
+	int i2 = objInteger;
+	System.out.println("auto-unboxing i2 : " + i2 );
+	
+	//byte b = objInteger;// auto-unboxing vers un byte  NOK// erreur:type mismatch
+	//byte b2 = (byte )objInteger; //avec un cast en plus  NOK//erreur:cannot cast
+	
+	//en JAVA 4
 	int i = objInteger.intValue();
 	System.out.println("i : " + i );
-	
+	//intéressant
 	byte b = objInteger.byteValue();
 	System.out.println("b : " + b );
 
-	//  à choisir : l'AUTOMATIQUE ;)
-	//unboxing automatique (auto-unboxing),  à utiliser pour alléger l'écriture
-	int i2 = objInteger;
-	//b = objInteger;
-	System.out.println("i2 : " + i2 );
-	
-	//byte b2 = (Byte )o;
-	
-	//System.out.println("i2 : " + i2 );
-	
+	System.out.println();
+	System.out.println();		
 	///////////////////////////////////////////////////
 	/////  RAPPEL sur les chaines de caractères
-	String chaine3 = String.format("%d", 16);
-	System.out.println("valeur de chaine3 :" + chaine3);
-	//String chaine4 = "" + 18;
+	///////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	//  METHODES STATIQUES DE LA CLASSE String
+	//String.
 	
 	String chaine5 = String.valueOf(19);
 	System.out.println("valeur de chaine5 :" + chaine5);
+	
+	// sans intérêts
+	//chaine5 = String.valueOf("19");
+	//System.out.println("valeur de chaine5 :" + chaine5);
+	//chaine5 = String.valueOf("coucou");
+	//System.out.println("valeur de chaine5 :" + chaine5);
+	
+	String chaine3 = String.format("%d", 16);
+	System.out.println("valeur de chaine3 :" + chaine3);
+	//String chaine4 = "" + 18;
 
+	///////////////////////////////////////////////////
+	//  TRAVAIL A REALISER
+	///////////////////////////////////////////////////
+	//Faites un schéma illustrant les passages d'un primitif
+	//à une chaine puis à un objet wrapper
 }
 }
